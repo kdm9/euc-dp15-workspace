abadd9cd0e35e84f33ec6aa7434ed8d943787431
Kevin Murray <kdmfoss@gmail.com>
2017-07-13 10:32:59 +1000

    Minor fixes etc.

--- workspace/raijin/modules.sh
+++ workspace/raijin/modules.sh
@@ -1,3 +1,3 @@
 module load snakemake adapterremoval nextgenmap samtools bcftools freebayes
-module load bwa angsd plink mash khmer kwip pigz
+module load bwa angsd plink mash khmer kwip pigz seqhax
 
--- workspace/raijin/submit.sh
+++ workspace/raijin/submit.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#PBS -q express
+#PBS -l ncpus=1
+#PBS -l walltime=24:00:00
+#PBS -l mem=8G
+#PBS -l jobfs=300G
+#PBS -l other=gdata1
+#PBS -l wd
+#PBS -M kevin@kdmurray.id.au
+#PBS -m abe
+#PBS -P xe2
+
 logdir=raijin/log
 if [ -d $logdir ]
 then
@@ -23,6 +34,9 @@ fi
 QSUB="qsub -q {cluster.queue} -l ncpus={threads} -l jobfs={cluster.jobfs}"
 QSUB="$QSUB -l walltime={cluster.time} -l mem={cluster.mem} -N {cluster.name}"
 QSUB="$QSUB -l wd -o $logdir -e $logdir -P xe2"
+torun="${2:-all}"
+
+snakemake --unlock
 
 snakemake                                \
     -j 500                               \
@@ -30,4 +44,4 @@ snakemake                                \
     --js raijin/jobscript.sh             \
     --rerun-incomplete                   \
     --keep-going                         \
-    --cluster "$QSUB" $@
+    --cluster "$QSUB" $torun
