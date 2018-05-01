c29cbeec1176df3f6d03acc00a54ca44f80c0282
Kevin Murray <kdmfoss@gmail.com>
2018-05-02 09:48:23 +1000

    Correct some issues with raijin scripts

--- workspace/raijin/submit.sh
+++ workspace/raijin/submit.sh
@@ -1,46 +1,39 @@
 #!/bin/bash
 #PBS -q normal
 #PBS -l ncpus=1
-#PBS -l walltime=24:00:00
-#PBS -l mem=10G
-#PBS -l jobfs=400G
+#PBS -l walltime=72:00:00
+#PBS -l mem=5G
 #PBS -l other=gdata1
 #PBS -l wd
 #PBS -M kevin@kdmurray.id.au
 #PBS -m abe
 #PBS -P xe2
+set -xe
 
 logdir=raijin/log
-if [ -d $logdir ]
-then
-  pushd $logdir >/dev/null
-  if [ -n "$(ls *.ER *.OU 2>/dev/null)" ]
-  then
-    tar cf `date +%y%m%d_%H%M%S`.tar *.OU *.ER 2>/dev/null
-    rm *.OU *.ER
-  fi
-  popd >/dev/null
-else
-  mkdir -p $logdir
-fi
-
+mkdir -p $logdir
+mkdir -p data/log/
 source raijin/modules.sh
+export TMPDIR=${PBS_JOBFS:-$TMPDIR}
+
+TARGET=${TARGET:-all}
+SNAKEFILE=${SNAKEFILE:-Snakefile}
 
 QSUB="qsub -q {cluster.queue} -l ncpus={threads} -l jobfs={cluster.jobfs}"
 QSUB="$QSUB -l walltime={cluster.time} -l mem={cluster.mem} -N {cluster.name}"
 QSUB="$QSUB -l wd -o $logdir -e $logdir -P xe2"
 
-mkdir -p data/log/
 snakemake --unlock
 
-snakemake                                 \
-    -j 1000                               \
-    --cluster-config raijin/cluster.yaml  \
-    --local-cores ${PBS_NCPUS:-16}        \
-    --js raijin/jobscript.sh              \
-    --rerun-incomplete                    \
-    --keep-going                          \
-    --snakefile "${snakefile:-Snakefile}" \
-    --cluster "$QSUB"                     \
-    "${target:-all}"                      \
-    >data/log/${PBS_JOBID:-}snakemake.log 2>&1
+snakemake                                                 \
+    -j 1000                                               \
+    --cluster-config raijin/cluster.yaml                  \
+    --local-cores ${PBS_NCPUS:-1}                         \
+    --js raijin/jobscript.sh                              \
+    --rerun-incomplete                                    \
+    --keep-going                                          \
+    --snakefile "$SNAKEFILE"                              \
+    --cluster "$QSUB"                                     \
+    "$TARGET"                                             \
+    >data/log/submitter_${PBS_JOBID:-}_snakemake.log 2>&1 \
+
