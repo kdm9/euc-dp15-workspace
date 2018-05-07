18c75734f14053912dc7d04f865bc00ee5a4e077
Kevin Murray <kdmfoss@gmail.com>
2018-05-07 13:55:25 +1000

    snakemake: produced filtered VCFs

--- workspace/raijin/modules.sh
+++ workspace/raijin/modules.sh
@@ -1,2 +1,2 @@
-module load snakemake adapterremoval nextgenmap/0.5.5 samtools bcftools freebayes
+module load snakemake adapterremoval nextgenmap/0.5.5 samtools bcftools htslib freebayes
 module load bwa angsd plink mash khmer kwip pigz seqhax R/3.3.0 stampy sra-toolkit
