6b92b422580a645bcf07a99f3bf625bd4b8f61e1
Kevin Murray <kdmfoss@gmail.com>
2018-07-28 15:33:55 +1000

    Remove sandbox angsd/pcangsd pipeline

--- workspace/sandbox/pcangsd/03_submit-subsetsV2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-NREPS=100
-
-for i in $(seq 0 10 100)
-do
-    TO=$(($i + 9))
-    qsub -v REPFROM=${i},REPTO=${TO} 03_subsetV2.pbs
-done
--- workspace/sandbox/pcangsd/04_submit-pcangsd.sh
+++
@@ -1,5 +0,0 @@
-#for i in $(seq 0 9)
-for i in $(seq 10 109)
-do
-    qsub -v REP=$(printf "%03d\n" $i) 04_pcangsd.pbs
-done
